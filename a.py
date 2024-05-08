7cf26cddfd0d0fff66c55bc900f1618c33e5906b
kimhj <kimhj0419@gmail.com>
2024-05-08 21:05:25 +0900

    칩 / 배팅관련 240508

--- a.py
+++ a.py
@@ -1,73 +1,63 @@
-import time
-import subprocess
-import pyautogui
-import pyperclip
+import numpy as np
+base = 3000
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
-
-options = ChromeOptions()
-user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-options.add_argument('user-agent=' + user_agent)
-options.add_argument("lang=ko_KR")
-options.add_argument('--window-size=1920,1020')
-options.add_argument("--no-sandbox")
-options.add_experimental_option("detach", True)
-
-
-# 크롬 드라이버 최신 버전 설정
-service = ChromeService(executable_path=ChromeDriverManager().install())
-
-# chrome driver
-driver = webdriver.Chrome(service=service, options=options) # <- options로 변경
-
-# 초기 페이지로 이동
-driver.get("https://www.lan5445.com")
-
-last_window_handle = driver.current_window_handle
-
-# 특정 파라미터가 포함된 URL을 찾은 후 업데이트를 중지하기 위한 플래그
-update_completed = False
-
-while True:
-    # 업데이트가 완료된 경우 루프 중지
-    if update_completed:
-       break
-
-    try:
-        # 현재 열려 있는 모든 창의 핸들 가져오기
-        window_handles = driver.window_handles
-
-        # 새 창이 열렸는지 확인
-        for window_handle in window_handles:
-            if window_handle != last_window_handle:
-                # 새로 열린 창으로 전환
-                driver.switch_to.window(window_handle)
-
-                # 새 창의 URL 확인
-                current_url = driver.current_url
-
-                # URL에 특정 파라미터가 포함되어 있는지 확인
-                if "game=baccarat" in current_url:
-                    print("특정 파라미터가 포함된 새 창 URL:", current_url)
-                    time.sleep(3)
-                    driver.switch_to.frame(driver.find_element(By.TAG_NAME,"iframe"))
-                    time.sleep(3)
-                    elem = driver.find_element(By.CLASS_NAME, 'roadGrid--bd5fc')
-                    text_to_input = elem.get_attribute('outerHTML')
-                    pyperclip.copy(text_to_input)
-                    subprocess.Popen(['notepad.exe'])
-                    time.sleep(2)  # 메모장이 실행될 때까지 잠시 대기
-                    pyautogui.hotkey('ctrl', 'v')
-
-                    update_completed = True
+multiply = 0
+a1 = 1
+a2 = 1
+a3 = 2
+a4 = 3
+a5 = 4
+a6 = 6
+a7 = 8
+a8 = 11
+a9 = 15
+a10 = 20
+a11 = 30
+a12 = 40
+a13 = 60
+a14 = 80
+a15 = 120
+a16 = 160
+a17 = 240
+a18 = 320
+a19 = 480
+a20 = 640
 
 
-        # 리소스 사용 최소화를 위해 잠시 대기
-        time.sleep(1)
-    except KeyboardInterrupt:
-        # 사용자가 Ctrl+C를 누르면 루프 종료
-        break
+def chip_selection(price):
+    # 칩 값과 이름을 튜플 리스트로 저장
+    chips = [
+        (500000, '6'),
+        (100000, '5'),
+        (25000, '4'),
+        (5000, '3'),
+        (2000, '2'),
+        (1000, '1')
+    ]
+
+    # 결과를 저장할 문자열
+    result = []
+
+    for value, name in chips:
+        if price >= value:
+            count = price // value  # 해당 칩으로 몇 개 살 수 있는지 계산
+            price %= value  # 남은 금액 계산
+            if count > 0:
+                result.append(f"{name}번칩 {int(count)}개")
+
+    # 결과 출력
+    print(", ".join(result))
+    print(result)
+
+def abc():
+    global multiply
+
+    multiply = base / a1
+
+    return multiply
+
+m = abc()
+
+print(a1*m, a2*m, a3*m, a4*m, a5*m, a6*m, a7*m, a8*m, a9*m, a10*m, a11*m, a12*m, a13*m, a14*m, a15*m, a16*m, a17*m, a18*m, a19*m, a20*m)
+print(chip_selection(a20*m))
+
